4af69a4465fa78f7eb9ff8d369461d315f344cb1
Tuatini Godard <tuatinigodard@gmail.com>
2020-10-26 12:44:40 +0400

    Update scriptv1.sh

--- scriptv1.sh
+++ scriptv1.sh
@@ -24,7 +24,7 @@ sudo chmod +x /usr/local/bin/aws-iam-authenticator
 
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 
-sudo mv -v /tmp/eksctl /usr/local/bin
+sudo mv -v /tmp/eksctl /bin/
 eksctl version
 
 eksctl completion bash >> ~/.bash_completion
